b4d9ba8cee0c60acf17e71c0a229f53a0a69ac94
Nishad <er.nishadnk@gmail.com>
2024-04-03 09:23:01 +0530

    last commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello World02\n'
+  const msg = 'Hello World002\n'
   res.end(msg);
 });
 
